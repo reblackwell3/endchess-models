fc3d5f3cac2b2b68a24dbadf03124845e062f471
Robert Blackwell <reblackwell3@gmail.com>
2024-09-09 17:33:31 -0400

    add index and index.d.ts

---
+++ src/index.d.ts
@@ -0,0 +1,11 @@
+// src/index.d.ts
+
+export { default as Game } from './models/raw/gameModel';
+export { default as Puzzle } from './models/raw/puzzleModel';
+
+export { default as SystemGameData } from './models/system/systemGameDataModel';
+export { default as SystemImportData } from './models/system/systemImportDataModel';
+
+export { default as Rating } from './models/user/ratingModel';
+export { default as UserEvent } from './models/user/userEventModel';
+export { default as User } from './models/user/userModel';
