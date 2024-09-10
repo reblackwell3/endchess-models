28c655017ba10ccda5d3120f5d196825b692160f
Robert Blackwell <reblackwell3@gmail.com>
2024-09-10 11:48:12 -0400

    update index and declaration

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,10 @@
 // src/index.ts
 
+export * from './models/raw/analysisModel';
 export * from './models/raw/gameModel';
 export * from './models/raw/puzzleModel';
 
-export * from './models/system/systemGameDataModel';
 export * from './models/system/systemImportDataModel';
 
-export * from './models/user/ratingModel';
-export * from './models/user/userEventModel';
+export * from './models/user/playerDataModel';
 export * from './models/user/userModel';
