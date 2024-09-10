6333c71b9d0a6e12aad2129748b511fe50217dbf
Robert Blackwell <reblackwell3@gmail.com>
2024-09-10 14:01:56 -0400

    update to camel case

--- src/models/raw/gameModel.ts
+++ src/models/raw/gameModel.ts
@@ -2,17 +2,17 @@ import { Document, Model, model, Schema, Types } from 'mongoose';
 
 export interface IGame extends Document {
   _id: Types.ObjectId;
-  import_from: string; // 'chess.com' or 'lichess'
+  importFrom: string; // 'chess.com' or 'lichess'
   url: string;
   pgn: string;
-  time_control: string;
-  end_time: number;
+  timeControl: string;
+  endTime: number;
   rated: boolean;
   tcn?: string;
   uuid: string;
-  initial_setup: string;
+  initialSetup: string;
   fen: string;
-  time_class: string;
+  timeClass: string;
   rules: string;
   eco?: string;
   white: {
@@ -29,17 +29,17 @@ export interface IGame extends Document {
 }
 
 const gameSchema = new Schema<IGame>({
-  import_from: { type: String, required: true },
+  importFrom: { type: String, required: true },
   url: { type: String, required: true },
   pgn: { type: String, required: true },
-  time_control: { type: String, required: true },
-  end_time: { type: Number, required: true },
+  timeControl: { type: String, required: true },
+  endTime: { type: Number, required: true },
   rated: { type: Boolean, required: true },
   tcn: { type: String },
   uuid: { type: String, required: true },
-  initial_setup: { type: String },
+  initialSetup: { type: String },
   fen: { type: String },
-  time_class: { type: String, required: true },
+  timeClass: { type: String, required: true },
   rules: { type: String, required: true },
   eco: { type: String },
   white: {
