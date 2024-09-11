07505af5acfefc8bd0d098936092a7dd21615033
Robert Blackwell <reblackwell3@gmail.com>
2024-09-11 18:50:05 -0400

    remove dist, add ipuzzle (#2)

--- src/models/raw/puzzleModel.ts
+++ src/models/raw/puzzleModel.ts
@@ -1,17 +1,35 @@
-// backend/puzzles/puzzleModel.js
-import mongoose from 'mongoose';
+import mongoose, { Document, Schema, Model } from 'mongoose';
 
-const puzzleSchema = new mongoose.Schema({
-  PuzzleId: String,
-  FEN: String,
-  Moves: String,
-  Rating: Number,
-  RatingDeviation: Number,
-  Popularity: Number,
-  NbPlays: Number,
-  Themes: String,
-  GameUrl: String,
-  OpeningTags: String,
+// Define the Puzzle interface extending Document
+export interface IPuzzle extends Document {
+  puzzleId: string;
+  fen: string;
+  moves: string;
+  rating: number;
+  ratingDeviation: number;
+  popularity: number;
+  nbPlays: number;
+  themes: string;
+  gameUrl: string;
+  openingTags: string;
+}
+
+// Define the Puzzle schema
+const puzzleSchema = new Schema<IPuzzle>({
+  puzzleId: { type: String, required: true },
+  fen: { type: String, required: true },
+  moves: { type: String, required: true },
+  rating: { type: Number, required: true },
+  ratingDeviation: { type: Number, required: true },
+  popularity: { type: Number, required: true },
+  nbPlays: { type: Number, required: true },
+  themes: { type: String, required: true },
+  gameUrl: { type: String, required: true },
+  openingTags: { type: String, required: true },
 });
 
-export const Puzzle = mongoose.model('Puzzle', puzzleSchema);
+// Create the Puzzle model
+export const Puzzle: Model<IPuzzle> = mongoose.model<IPuzzle>(
+  'Puzzle',
+  puzzleSchema,
+);
